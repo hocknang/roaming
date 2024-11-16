c5a20b45fb8b103f7a36646352358acd611e8466
peiyuan <fang_peiyuan@yahoo.com>
2024-11-16 19:45:42 +0800

    new

--- main.py
+++ main.py
@@ -17,6 +17,14 @@ def home():
 
     st.write("Script & Key Status")
 
+    image_path1 = 'https://raw.githubusercontent.com/hocknang/roaming/refs/heads/master/resource/Script%20for%20Ideafest%20as%20of%2015%20Nov%202024_Page_1.jpg'
+
+    # Define the HTML hyperlink with the image
+    html_string = f'<a href="{image_path}" target="_blank"><img src="{image_path1}" caption="legend"></a>'
+
+    # Display the image using `st.markdown`
+    st.markdown(html_string, unsafe_allow_html=True)
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
