b7972eb0548c4f4c0b428ef7402f723996361276
peiyuan <fang_peiyuan@yahoo.com>
2024-11-16 19:53:47 +0800

    new

--- main.py
+++ main.py
@@ -7,42 +7,27 @@ import streamlit as st
 def home():
     st.write("Presentation Slide")
 
-    image_path = 'https://raw.githubusercontent.com/hocknang/roaming/refs/heads/master/resource/Slide1.JPG'
-
-    # Define the HTML hyperlink with the image
-    html_string = f'<a href="{image_path}" target="_blank"><img src="{image_path}" caption="legend"></a>'
-
-    # Display the image using `st.markdown`
-    st.markdown(html_string, unsafe_allow_html=True)
+    print("https://raw.githubusercontent.com/hocknang/roaming/refs/heads/master/resource/Slide1.JPG")
 
     st.write("Script & Key Status")
 
     st.write("Page 1")
 
-    image_path1 = 'https://raw.githubusercontent.com/hocknang/roaming/refs/heads/master/resource/Script%20for%20Ideafest%20as%20of%2015%20Nov%202024_Page_1.jpg'
-
-    # Define the HTML hyperlink with the image
-    html_string = f'<a href="{image_path1}" target="_blank"><img src="{image_path1}" caption="legend"></a>'
-
-    # Display the image using `st.markdown`
-    st.markdown(html_string, unsafe_allow_html=True)
+    print("https://raw.githubusercontent.com/hocknang/roaming/refs/heads/master/resource/Script%20for%20Ideafest%20as%20of%2015%20Nov%202024_Page_1.jpg")
 
     st.write("Page 2")
 
-    image_path2 = 'https://raw.githubusercontent.com/hocknang/roaming/refs/heads/master/resource/Script%20for%20Ideafest%20as%20of%2015%20Nov%202024_Page_2.jpg'
-
-    # Define the HTML hyperlink with the image
-    html_string = f'<a href="{image_path2}" target="_blank"><img src="{image_path2}" caption="legend"></a>'
-
-    # Display the image using `st.markdown`
-    st.markdown(html_string, unsafe_allow_html=True)
+    print("https://raw.githubusercontent.com/hocknang/roaming/refs/heads/master/resource/Script%20for%20Ideafest%20as%20of%2015%20Nov%202024_Page_2.jpg")
 
     st.write("Page 3")
 
-    image_path3 = "https://raw.githubusercontent.com/hocknang/roaming/refs/heads/master/resource/Script%20for%20Ideafest%20as%20of%2015%20Nov%202024_Page_3.jpg"
+    print("https://raw.githubusercontent.com/hocknang/roaming/refs/heads/master/resource/Script%20for%20Ideafest%20as%20of%2015%20Nov%202024_Page_3.jpg")
 
+
+
+def print(image_path):
     # Define the HTML hyperlink with the image
-    html_string = f'<a href="{image_path3}" target="_blank"><img src="{image_path3}" caption="legend"></a>'
+    html_string = f'<a href="{image_path}" target="_blank"><img src="{image_path}" caption="legend"></a>'
 
     # Display the image using `st.markdown`
     st.markdown(html_string, unsafe_allow_html=True)
