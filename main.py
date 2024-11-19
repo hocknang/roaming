3dbcf4b28dbd9a59abbe0780b210ad34a21b1010
peiyuan <fang_peiyuan@yahoo.com>
2024-11-19 17:12:28 +0800

    new

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def home():
         # Redirect the user with a hidden URL
         st.markdown("""
             <script>
-            window.location.href = "https://plumber.gov.sg/tiles/9c5aeb0f-1db2-4a3a-a615-a2ea829ebfee/54f8b9b9-0bf1-4fe1-96e9-53091aab8eb4";
+            window.location.href = "https://www.example.com";
             </script>
             """, unsafe_allow_html=True)
 
