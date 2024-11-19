df647614fc3c1c65751430d835f6052c14a01004
peiyuan <fang_peiyuan@yahoo.com>
2024-11-19 16:51:22 +0800

    new

--- main.py
+++ main.py
@@ -3,12 +3,14 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import streamlit as st
-import streamlit.components.v1 as components
 
 def home():
     st.write("QR Code")
 
-    components.iframe("https://plumber.gov.sg/tiles/9c5aeb0f-1db2-4a3a-a615-a2ea829ebfee/54f8b9b9-0bf1-4fe1-96e9-53091aab8eb4")
+    st.write(
+        f'<iframe src="https://plumber.gov.sg/tiles/9c5aeb0f-1db2-4a3a-a615-a2ea829ebfee/54f8b9b9-0bf1-4fe1-96e9-53091aab8eb4"></iframe>',
+        unsafe_allow_html=True,
+    )
 
 
 
