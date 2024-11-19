749b2c914d6fb31851abcde311bad9d3c9a7e997
peiyuan <fang_peiyuan@yahoo.com>
2024-11-19 22:21:24 +0800

    new

--- main.py
+++ main.py
@@ -9,16 +9,12 @@ def home():
         <html>
 <body>
 
-<h2>Redirect to a Webpage</h2>
-<p>The replace() method replaces the current document with a new one:</p>
-
-<button onclick="myFunction()">Replace document</button>
-
-<script>
-function myFunction() {
-  location.replace("https://www.w3schools.com")
-}
-</script>
+ <button id="myButton">Click me to trigger JavaScript</button>
+    <script type="text/javascript">
+        document.getElementById("myButton").onclick = function() {
+            alert("Button clicked! You can trigger any JavaScript here.");
+        }
+    </script>
 
 </body>
 </html> 
