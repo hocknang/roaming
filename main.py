d523498e18e94f028458d57f4b48b0854d987425
peiyuan <fang_peiyuan@yahoo.com>
2024-11-19 22:02:42 +0800

    new

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
 
     home()
-s
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
