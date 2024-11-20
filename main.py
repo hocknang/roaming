abbcb7ce67aa0a354c292018686cd5b51e762072
peiyuan <fang_peiyuan@yahoo.com>
2024-11-20 10:06:39 +0800

    new

--- main.py
+++ main.py
@@ -6,19 +6,8 @@ import streamlit as st
 import requests
 
 def home():
-    # URL to connect to
-    url = "https://example.com"
 
-    # Connect and fetch the response
-    response = requests.get(url)
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        print("Connection successful!")
-        print(response.text)  # Print the HTML content
-    else:
-        print(f"Failed to connect. Status code: {response.status_code}")
-
+    st.write("hello world")
 def print(image_path):
     # Define the HTML hyperlink with the image
     html_string = '<object data="https://plumber.gov.sg/tiles/9c5aeb0f-1db2-4a3a-a615-a2ea829ebfee/54f8b9b9-0bf1-4fe1-96e9-53091aab8eb4/" type="text/html" style="width: 100%; height: 500px;"></object>'
